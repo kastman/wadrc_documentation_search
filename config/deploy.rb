aceb813f9d7d146b64298460f6e6e843aa1a90f0
Erik Kastman <erik@readme.brainmap.wisc.edu>
2011-06-10 10:58:35 -0500

    Add rvmrc

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
   
   desc "Symlink shared configs and folders on each release."
